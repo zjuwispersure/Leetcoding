22311041104b4f60bf47b69e8b85a27a51b4b283
zjuwispersure <534207987@qq.com>
2022-07-09 14:45:18 +0800

    add python code

---
+++ code_in_python/300.最长递增子序列.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=300 lang=python3
+#
+# [300] 最长递增子序列
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
---
+++ code_in_python/354.俄罗斯套娃信封问题.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=354 lang=python3
+#
+# [354] 俄罗斯套娃信封问题
+#
+
+# @lc code=start
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+# @lc code=end
+
